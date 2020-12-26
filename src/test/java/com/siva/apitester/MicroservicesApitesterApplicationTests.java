a65a88bbe88fae87ba36bc752a7c4b585ccd753d
siva saravanan <sivacham@gmail.com>
2020-12-26 21:42:06 +0530

    initial commit

---
+++ src/test/java/com/siva/apitester/MicroservicesApitesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.apitester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApitesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
